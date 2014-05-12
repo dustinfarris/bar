cf64b30a6bbb0e61340242e9b5145e827e3b38ec
Dustin Farris <dustin@dustinfarris.com>
2014-05-12 10:53:53 -0400

    Add comments

--- app/app.js
+++ app/app.js
@@ -1,6 +1,7 @@
 import Resolver from 'ember/resolver';
 import loadInitializers from 'ember/load-initializers';
 
+// Set this to true for tests to fail
 Ember.MODEL_FACTORY_INJECTIONS = false;
 
 var App = Ember.Application.extend({
